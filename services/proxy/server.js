fb4d5e37741bcd322dd7c25c93640a29a0a26c90
Abraham Esparza <abrahamesparza.us@gmail.com>
2020-03-31 21:07:00 -0700

    build proxy. working on rendering services

--- services/proxy/server.js
+++ services/proxy/server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const path = require('path')
 
 const app = express();
-const port = 3000;
+const port = 3500;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(port, () => {
   console.log(`Live on server: ${port}`);
-})
+});
